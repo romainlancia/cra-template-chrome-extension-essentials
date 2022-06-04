1c21a7b13c0db9016c33ef11fd719efe2f7aa670
Romain Lancia <romainlancia@gmail.com>
2022-06-04 18:33:22 +0200

    feat: cra template with typescript, tailwind, husky, lint-staged, prettier installed and configured

---
+++ template/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='bg-indigo-600 flex items-center justify-center h-screen'>
+      <div className='cursor-pointer .antialiased p-5 shadow-xl rounded-full bg-white font-sans font-bold capitalize text-2xl text-black'>
+        Hello World⚡
+      </div>
+    </div>
+  );
+}
+
+export default App;
